88d8e982b55a9fdf28f8320bf64791961800ec19
EFFYLYX <40695879+EFFYLYX@users.noreply.github.com>
2022-03-23 22:26:13 +0800

    Update binary_tree.py

--- data_structure/binary_tree.py
+++ data_structure/binary_tree.py
@@ -250,3 +250,15 @@ class Solution:
                 return -1
             return max(l,r)+1
         return helper(root) >= 0
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root: return True
+        return abs(self.depth(root.left) - self.depth(root.right)) <= 1 and \
+            self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def depth(self, root):
+        if not root: return 0
+        return max(self.depth(root.left), self.depth(root.right)) + 1
+
+
